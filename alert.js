91c50c373108100b0529e004c1d6694bef50d57d
Syed Humza <105381562+syedhumzasalman@users.noreply.github.com>
2024-12-17 02:33:21 -0800

    first commit

---
+++ alert.js
@@ -0,0 +1,156 @@
+// charter 01   ALERTS 
+
+
+// QUESTION 01
+
+function q1() {
+    alert("Welcome to my website! Have a great day!");
+}
+
+// QUESTION 02
+
+function q2() {
+    alert("Error! Please enter a valid password.");
+}
+// QUESTION 03
+
+function q3() {
+    alert("Welcome to js land... \n Happy Coding!")
+}
+// QUESTION 04
+
+function q4() {
+    alert("Welcome to Js Land...")
+    alert("Happy Coding! \n prevent this page from creating addition dialogs.")
+}
+// QUESTION 05
+
+function q5() {
+    alert("Hello I can run JS through my web browser's console")
+}
+
+
+
+
+
+// charter 02 VARIABLES FOR STRINGS 
+
+
+// QUESTION 01
+
+function username(){
+    var username = "humza";
+    alert(username);
+}
+
+
+
+// QUESTION 02
+
+function fullname(){
+    var fullname = "Syed Humza Salman";
+    alert(fullname);
+}
+
+
+
+// QUESTION 03
+
+function message(){
+    var message = "Hello World";
+    alert(message);
+}
+
+
+// QUESTION 04
+
+function bio() {
+    alert("Syed Humza Salman")
+    alert("I'm 25 Year Old")
+    alert("Student of SMIT (Saylani Mass IT Training)")
+}
+
+
+// QUESTION 05
+
+function pizza() {
+    alert("pizza \n pizz \n piz \n pi \n p")
+}
+
+
+
+
+// QUESTION 06
+
+function email() {
+    var email = "syedhumzasalman@gmail.com";
+    alert("my email address is " + email)
+}
+
+
+// QUESTION 07
+
+function book() {
+    var book = "A smarter way to learn JavaScript";
+    alert("I'm Trying to learn from the Book " + book)
+}
+
+
+// QUESTION 08
+
+function html() {
+  document.write("yes! I can write HTML Content through JavaScript")
+}
+
+
+// QUESTION 09
+
+function fun() {
+    var  fun = "'══════════ஜ۩۞۩ஜ══════════'";
+    alert(fun)
+}
+
+function funb() {
+    var  funb = "'══════════ஜ۩۞۩ஜ══════════'";
+    document.write(funb)
+}
+
+
+
+
+
+// Charter 03  VARIABLES FOR NUMBERS 
+
+
+// QUESTION 01  
+
+function age() {
+    var age = 25;
+    alert(age);
+}
+
+
+// QUESTION 02
+
+function time() {
+    alert("you have visit this site 15 times")
+}
+
+
+
+// QUESTION 03
+
+function day(){
+    var birthYear = 1999;
+    document.write ("My birth year is " + birthYear);
+}
+
+
+
+// QUESTION 04
+function store(){
+var visitorName = "Syed Humza Salman";
+var productTitle = "T-shirt";
+var quantity = 5;
+document.write (visitorName + " Ordered " + quantity +  productTitle +   " (S) on XYZ Clothing Store")
+}
