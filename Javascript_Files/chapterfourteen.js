d12e0aaf5d107f1df1d2baac56a63407e264e145
Syed Humza <105381562+syedhumzasalman@users.noreply.github.com>
2024-12-26 17:05:59 +0500

    Added Chapter 14

---
+++ Javascript_Files/chapterfourteen.js
@@ -0,0 +1,38 @@
+// chapter 14 ARRAYS
+
+
+// Question 01 Declare an empty array using JS literal notation to store student names in future?
+console.log("1. Declare an empty array using JS literal notation to store student names in future")
+
+let emptyArray = [];
+console.log(emptyArray.length);
+
+// Question 02 Declare an empty array using JS object notation to store student names in future.?
+console.log("2. Declare an empty array using JS object notation to store student names in future.")
+
+let students = {
+    names: []
+  };
+  console.log(students.length);
+
+// Question 03 Declare and initialize a strings array?
+
+console.log("3. Declare and initialize a strings array?")
+let string = ["Ahmed", "Humza", "Bilal", "Ali"];
+console.log(string)
+
+
+
+// Question 04  Declare and initialize a numbers array?
+console.log("4. Declare and initialize a numbers array?")
+let number = [1 ,  2  ,28  , 29  ,89  , 30 , 20];
+console.log(number)
+
+
+//Question 05 Declare and initialize a boolean array?
+console.log("5.Declare and initialize a boolean array?")
+
+
+
+
+
